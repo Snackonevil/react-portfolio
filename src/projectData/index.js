335f0dffa7605fb973fedd533065301ec8983663
Kevin Lacson <lacsonky@gmail.com>
2022-04-28 20:03:37 -0400

    add tripiter to projects

--- src/projectData/index.js
+++ src/projectData/index.js
@@ -1,41 +1,50 @@
-const meteomelodiesImg = require('./meteo-melodies.png');
-const modularImg = require('./modular-screenshot.png');
-const weatherDashImg = require('./weather-dash.png');
-const elderRavenImg = require('./elder-raven-logo.png');
+const meteomelodiesImg = require("./meteo-melodies.png");
+const modularImg = require("./modular-screenshot.png");
+const weatherDashImg = require("./weather-dash.png");
+const elderRavenImg = require("./elder-raven-logo.png");
+const tripiterLogo = require("./tripiter-logo.png");
 
 const projectData = [
-  {
-    id: 1,
-    name: 'MeteoMelodies',
-    description: 'This is a description of MeteoMelodies',
-    appLink: 'https://acuson.github.io/meteo-melodies/',
-    codeLink: 'https://github.com/acuson/meteo-melodies',
-    image: meteomelodiesImg,
-  },
-  {
-    id: 2,
-    name: 'Modular',
-    description: 'This is a description of Modular',
-    appLink: 'https://modular-tech-blog.herokuapp.com/login',
-    codeLink: 'https://github.com/Snackonevil/modular-a-tech-blog',
-    image: modularImg,
-  },
-  {
-    id: 3,
-    name: 'Weather Dashboard',
-    description: 'This is a description of Weather Dashboard',
-    appLink: 'https://snackonevil.github.io/weatherdash-react-rebuild/',
-    codeLink: 'https://github.com/Snackonevil/weatherdash-react-rebuild',
-    image: weatherDashImg,
-  },
-  {
-    id: 4,
-    name: 'Elder Raven',
-    description: 'This is a description of Elder Raven',
-    appLink: 'https://snackonevil.github.io/elder-raven-mockup/',
-    codeLink: 'https://github.com/Snackonevil/elder-raven-mockup',
-    image: elderRavenImg,
-  },
+    {
+        id: 1,
+        name: "MeteoMelodies",
+        description: "This is a description of MeteoMelodies",
+        appLink: "https://acuson.github.io/meteo-melodies/",
+        codeLink: "https://github.com/acuson/meteo-melodies",
+        image: meteomelodiesImg,
+    },
+    {
+        id: 2,
+        name: "Modular",
+        description: "This is a description of Modular",
+        appLink: "https://modular-tech-blog.herokuapp.com/login",
+        codeLink: "https://github.com/Snackonevil/modular-a-tech-blog",
+        image: modularImg,
+    },
+    {
+        id: 3,
+        name: "Weather Dashboard",
+        description: "This is a description of Weather Dashboard",
+        appLink: "https://snackonevil.github.io/weatherdash-react-rebuild/",
+        codeLink: "https://github.com/Snackonevil/weatherdash-react-rebuild",
+        image: weatherDashImg,
+    },
+    {
+        id: 4,
+        name: "Elder Raven",
+        description: "This is a description of Elder Raven",
+        appLink: "https://snackonevil.github.io/elder-raven-mockup/",
+        codeLink: "https://github.com/Snackonevil/elder-raven-mockup",
+        image: elderRavenImg,
+    },
+    {
+        id: 5,
+        name: "Tripiter",
+        description: "This is a description of Elder Raven",
+        appLink: "https://tripiter.herokuapp.com/",
+        codeLink: "https://github.com/Snackonevil/tripiter",
+        image: tripiterLogo,
+    },
 ];
 
 export default projectData;
